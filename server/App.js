60d15f15f8d26193004475ddcb30915fde195f0f
yomae <goldrunkim@naver.com>
2022-03-04 16:32:28 +0900

    init setting

---
+++ server/App.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use("/", (req, res) => {
+  res.send("Hello WaaT!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}...`);
+});
