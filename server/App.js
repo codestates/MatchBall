696fd43723dfa78bf957c1c00985a25fb9faa63c
fireballer150 <cutterballer@gmail.com>
2022-03-06 12:24:00 +0900

    server&mysqlRDSTest1

--- server/App.js
+++ server/App.js
@@ -1,8 +1,26 @@
 require("dotenv").config();
 const express = require("express");
+const mysql = require('mysql');
 const app = express();
 const cors = require("cors");
 const PORT = 80;
+
+const connection = mysql.createConnection({
+  host : 'matchball-database.cswrwl4zmldq.ap-northeast-2.rds.amazonaws.com',
+  user: 'admin',
+  password : 'matchballdatabase',
+  database : 'test',
+  port : '13306'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM users',(error, rows ,fields) =>{
+  if(error) throw error;
+  console.log('User info is',rows);
+})
+connection.end();
+
 
 console.log("hello");
 
