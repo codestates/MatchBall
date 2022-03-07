949504f60396e92181f668f9bdd84060a6641ddb
fireballer150 <cutterballer@gmail.com>
2022-03-07 15:39:45 +0900

    server setting database migration and connection1

--- server/App.js
+++ server/App.js
@@ -11,23 +11,23 @@ const loginRouter = require("./routes/loginReg");
 const matchRouter = require("./routes/matches");
 const mypageRouter = require("./routes/mypage");
 
-const connection = mysql.createConnection({
-  host : 'matchball-database.cswrwl4zmldq.ap-northeast-2.rds.amazonaws.com',
-  user: 'admin',
-  password : 'matchballdatabase',
-  database : 'test',
-  port : '13306'
-});
+// const connection = mysql.createConnection({
+//   host : 'matchball-database.cswrwl4zmldq.ap-northeast-2.rds.amazonaws.com',
+//   user: 'admin',
+//   password : 'matchballdatabase',
+//   database : 'test',
+//   port : '13306'
+// });
 
-connection.connect();
+// connection.connect();
 
-let testDummyData
-connection.query('SELECT * FROM users',(error, rows ,fields) =>{
-  if(error) throw error;
-  console.log('User info is',rows);
-  testDummyData = rows;
-})
-connection.end();
+// let testDummyData
+// connection.query('SELECT * FROM users',(error, rows ,fields) =>{
+//   if(error) throw error;
+//   console.log('User info is',rows);
+//   testDummyData = rows;
+// })
+// connection.end();
 
 console.log("hello!");
 
--- server/controllers/matches/list.js
+++ server/controllers/matches/list.js
@@ -1,3 +1,24 @@
+const mysql = require('mysql')
+// const app = express();
+const connection = mysql.createConnection({
+    host : 'matchball-database.cswrwl4zmldq.ap-northeast-2.rds.amazonaws.com',
+    user: 'admin',
+    password : 'matchballdatabase',
+    database : 'test',
+    port : '13306'
+  });
+  
+  connection.connect();
+  
+  let testDummyData
+  connection.query('SELECT * FROM matches',(error, rows ,fields) =>{
+    if(error) throw error;
+    console.log('Match info is!!',rows);
+    testDummyData = rows;
+  })
+  connection.end();
+  
+
 module.exports = (req, res) => {
-    res.send("matches list")
+    res.json(testDummyData)
 }
