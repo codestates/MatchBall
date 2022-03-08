4b649befeefd58970bb8d20e3217f748fdfb9316
yomae <goldrunkim@naver.com>
2022-03-08 19:10:16 +0900

    Feat: login and auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,79 @@
 import "./App.css";
-import Nav from "./components/Nav";
-import Footer from "./components/Footer";
 import Matches from "./pages/Matches";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
-import ResisterForm from "./components/RegisterForm";
 import UserInfoEditPage from "./pages/UserInfoEditPage";
 import MyPage from "./pages/MyPage";
 import RegisterPage from "./pages/RegisterPage";
-import RegisterEdigPage from "./pages/RegisterEdigPage";
 import Contents from "./components/Contents";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [matchData, setMatchData] = useState([]);
+  const [userAccessToken, setUserAccessToken] = useState("");
+  // const history = useHistory();
+
+  const isAuthenticated = () => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/matches/list`, {
+        cookie: userAccessToken,
+      })
+      .then((res) => {
+        setMatchData(res.data);
+        setIsLogin(true);
+        // history.push("/matches");
+      });
+  };
+
+  const handleResponseSuccess = () => {
+    isAuthenticated();
+  };
+
+  const handleLogout = () => {
+    axios.post(`${process.env.REACT_APP_API_URL}/users/signout`).then((res) => {
+      setIsLogin(false);
+      // history.push("/signin");
+    });
+  };
+
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
           <LandingPage />
         </Route>
+
         <Route path="/signin">
-          <LoginPage title="로그인" />
+          <LoginPage
+            title="로그인"
+            handleResponseSuccess={handleResponseSuccess}
+            setUserAccessToken={setUserAccessToken}
+          />
         </Route>
+
         <Route path="/signup">
           <SignUpPage />
         </Route>
+
         <Route path="/mypage">
-          <MyPage />
+          {isLogin ? (
+            // <MyPage handleLogout={handleLogout} />
+            <Redirect to="/" />
+          ) : (
+            <Redirect to="/signin" />
+          )}
         </Route>
+
         <Route path="/matches/new">
           <RegisterPage />
         </Route>
+
         <Route path="/matches">
-          <Matches />
+          <Matches data={matchData} />
         </Route>
       </Switch>
     </BrowserRouter>
--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
-import UserEditForm from "../components/UserEditForm";
+import SignUpForm from "../components/SignUpForm";
 
 function SignUpPage(props) {
   return (
     <>
       <Nav />
-      <UserEditForm title="회원가입" click="회원가입" />
+      <SignUpForm title="회원가입" />
       <Footer />
     </>
   );
