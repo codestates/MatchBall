7f68cc1a7529f89d6f13cb3af29b5b6dde9f859d
fireballer150 <cutterballer@gmail.com>
2022-03-09 14:52:28 +0900

    server client login issue ing

--- server/controllers/matches/list.js
+++ server/controllers/matches/list.js
@@ -3,17 +3,17 @@ const { users } = require('../../models')
 let userData=[];
 
 module.exports = (req, res) => {
-const params = 1
- matches.query(`
-  SELECT m.id,m.message,m.region,m.region_Detail,m.sitename,m.user_id,m.matchdate,m.is_matched,u.id,u.email,u.mobile,u.nickname,u.level,u.team 
-  FROM matches as m 
-  LEFT JOIN users as u 
-  ON m.user_id = u.id 
-  `,params,(err,result)=> res.json(result))
+// const params = 1
+//  matches.query(`
+//   SELECT m.id,m.message,m.region,m.region_Detail,m.sitename,m.user_id,m.matchdate,m.is_matched,u.id,u.email,u.mobile,u.nickname,u.level,u.team 
+//   FROM matches as m 
+//   LEFT JOIN users as u 
+//   ON m.user_id = u.id 
+//   `,params,(err,result)=> res.json(result))
  
-  // matches.findAll({})
-  //   .then(data => 
-  //     {
-  //       return res.json(data)
-  //     })
+  matches.findAll({})
+    .then(data => 
+      {
+        return res.json(data)
+      })
 };
